12e65bd88bf7523b756f6b32dd9755c701632eec
mohamed-aziz <medazizknani@gmail.com>
2017-08-13 03:18:17 +0100

    Add event bus (shared vue instance between components not great but it works)

---
+++ frontend/src/components/Jobs.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div class="container">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Stream channel</th>
+                    <th>Start time UTC</th>
+                    <th>End time UTC</th>
+                    <th>File name</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+  </template>
+
+<script>
+
+export default {
+
+}
+        </script>
